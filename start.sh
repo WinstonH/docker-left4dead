331476beee6f413bb341798ab81578043833d9b1
WinstonH <bjdhjj@126.com>
2017-07-18 17:24:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@ echo "104.115.209.150 store.steampowered.com" >> /etc/hosts
 echo "104.115.209.150 steamcommunity.com" >> /etc/hosts
 cd /steamcmd
 ./steamcmd.sh +login anonymous +force_install_dir /l4d2 +app_update 222860 validate +quit
+cd /l4d2/left4dead2
 ./srcds_run -game left4dead2 +sv_steamgroup 29436146 +sv_steamgroup_exclusive 1
